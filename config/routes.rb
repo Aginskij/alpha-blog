d735ed77621beac5c68f9173c30bec6824d40674
Alex Aginskij <alex.aginskij@gmail.com>
2016-01-09 12:39:28 +0300

    homepage moved to root

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  get 'pages/homepage', to: 'pages#homepage'
+  root 'pages#homepage'
   get 'pages/about', to: 'pages#about'
 
   resources :articles
